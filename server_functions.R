90aa3600753e84a58799b6fde9e86bd009ad16fa
jmcartiles <jmcartiles@gmail.com>
2018-05-01 12:38:54 +0100

    Add conditional % labels in yaxis

--- server_functions.R
+++ server_functions.R
@@ -3,16 +3,30 @@
 # load necessary functions
 
 # dygraphs data with units in euros
-custom_dygraph <- function(df, euros) {
+custom_dygraph <- function(df, euros, is.variation = FALSE) {
+  
+  # if (is.variation == TRUE) {
+  #   
+  #   nums <- unlist(lapply(df, is.numeric))
+  #   min.value <- min(df[,nums])
+  #   max.value <- max(df[,nums])
+  #   axis.range <- c(min.value, max.value)
+  #   
+  # } else {
+  #   
+  #   axis.range <- NULL
+  #   
+  # }
   
   xts(df, as.Date(df$fecha, format = "%Y-%m-%d %H:%M:%OS")) %>%
     dygraph() %>%
     dyAxis(
       name = "y",
+      # valueRange = axis.range,
       valueFormatter = 'function(d){return d}',
-      axisLabelFormatter = ifelse(euros == TRUE,
-                                  'function(d){return Math.round(d/1e6) + " mill.(\u20ac)"}',
-                                  'function(d){return Math.round(d/1e6) + " mill."}')
+      axisLabelFormatter = ifelse(is.variation == TRUE, 'function(d){return (d) + " %"}',
+                                  ifelse(is.variation == FALSE & euros == TRUE, 'function(d){return Math.round(d/1e6) + " mill.(\u20ac)"}',
+                                  'function(d){return Math.round(d/1e6) + " mill."}'))
     ) %>%
     dyOptions(fillGraph = TRUE, fillAlpha = 0.1) %>%
     dyRangeSelector()
